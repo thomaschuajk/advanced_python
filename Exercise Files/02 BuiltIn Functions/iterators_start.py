a71a411ccac179acae38fc7d492e7a2ae9626f61
thomaschuajk <58562441+thomaschuajk@users.noreply.github.com>
2024-09-21 21:13:37 +0000

    built-in functions start

--- Exercise Files/02 BuiltIn Functions/iterators_start.py
+++ Exercise Files/02 BuiltIn Functions/iterators_start.py
@@ -5,17 +5,32 @@ def main():
     # define a list of days in English and French
     days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
     daysFr = ["Dim", "Lun", "Mar", "Mer", "Jeu", "Ven", "Sam"]
+    daysFr2 = ["Dim", "Lun", "Mar", "Mer", "Jeu", "Ven"]
 
     # TODO: use iter to create an iterator over a collection
+    # i = iter(days)
+    # print(next(i))
+    # print(next(i))
+    # print(next(i))
+    # print(next(i))
 
     # TODO: iterate using a function and a sentinel
-
+    # with open("Exercise Files/02 BuiltIn Functions/testfile.txt","r") as fp:
+    #     for line in iter(fp.readline,''):
+    #         print(line)
     # TODO: use regular interation over the days
-
+    # for m in range(len(days)):
+    #     print(m+1, days[m])
     # TODO: using enumerate reduces code and provides a counter
-
+    # for i,m in enumerate(days,start=1):
+    #     print(i,m)
     # TODO: use zip to combine sequences
+    # for m in zip(days,daysFr):
+    #     print(m)
 
+    # TODO: use enumerate and zip to combine sequences
+    for i,m in enumerate(zip(days,daysFr2),start=1):
+        print(i, m[0], "=", m[1], "in French")
 
 if __name__ == "__main__":
     main()
--- Exercise Files/02 BuiltIn Functions/utilfuncs_start.py
+++ Exercise Files/02 BuiltIn Functions/utilfuncs_start.py
@@ -3,16 +3,17 @@
 
 def main():
     # use any() and all() to test sequences for boolean values
-    list1 = [1, 2, 3, 0, 5, 6]
+    list1 = [1, 2, 3, 4, 5, 6]
     
     # TODO: any will return true if any of the sequence values are true
-    
+    print(any(list1))
     # TODO: all will return true only if all values are true
-    
+    print(all(list1))
     # TODO: min and max will return minimum and maximum values in a sequence
-    
+    print(f"Min value of list1: {min(list1)}")
+    print(f"Max value of list1: {max(list1)}")
     # TODO: Use sum() to sum up all of the values in a sequence
-    
+    print(f"Sum of all elements in List1: {sum(list1)}")
 if __name__ == "__main__":
     main()
     
